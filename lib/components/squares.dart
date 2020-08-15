8ae71e9697b075ea0f435de04568e028cde96bdd
Zeus3hd <ahdhani0@gmail.com>
2020-08-15 14:38:13 +0300

    create rectangle widget and graph widget that are rendered as background

--- lib/components/squares.dart
+++ lib/components/squares.dart
@@ -52,6 +52,24 @@ class _SquaresState extends State<Squares> {
           top: _squareOffsetY + _width * 1.6,
           left: _squareOffsetX + _width * 1.7,
         ),
+        Graph(
+          width: _width * 2,
+          image: 'assets/graph.png',
+          top: _squareOffsetY + _width * 1.6,
+          left: _squareOffsetX + _width * 3.5,
+        ),
+        Rectangle(
+          height: 15.0,
+          width: MediaQuery.of(context).size.width / 2,
+          left: MediaQuery.of(context).size.width / 2,
+          top: 10.0,
+        ),
+        Rectangle(
+          height: 15.0,
+          width: MediaQuery.of(context).size.width / 2,
+          left: 0,
+          top: MediaQuery.of(context).size.height / 1.06,
+        ),
       ],
     );
   }
@@ -87,6 +105,85 @@ class _SingleSquareState extends State<SingleSquare> {
           widget.image,
           semanticsLabel: null,
           width: widget.width,
+        ),
+      ),
+    );
+  }
+}
+
+class Graph extends StatefulWidget {
+  final double width;
+  final String image;
+  final double top;
+  final double left;
+  Graph(
+      {Key key,
+      @required this.width,
+      @required this.image,
+      @required this.top,
+      @required this.left})
+      : super(key: key);
+
+  @override
+  _GraphState createState() => _GraphState();
+}
+
+class _GraphState extends State<Graph> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 500),
+      top: widget.top,
+      left: widget.left,
+      child: Opacity(
+        opacity: 0.5,
+        child: Image.asset(
+          widget.image,
+          width: widget.width,
+        ),
+      ),
+    );
+  }
+}
+
+class Rectangle extends StatefulWidget {
+  final double width;
+  final double height;
+  final double top;
+  final double left;
+  Rectangle({
+    Key key,
+    @required this.width,
+    @required this.height,
+    @required this.top,
+    @required this.left,
+  }) : super(key: key);
+
+  @override
+  _RectangleState createState() => _RectangleState();
+}
+
+class _RectangleState extends State<Rectangle> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      left: widget.left,
+      top: widget.top,
+      duration: Duration(milliseconds: 500),
+      child: Container(
+        width: widget.width,
+        height: widget.height,
+        child: null,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(239, 71, 111, 1),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(239, 71, 111, 0.3),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: Offset(0, 0),
+            ),
+          ],
         ),
       ),
     );
