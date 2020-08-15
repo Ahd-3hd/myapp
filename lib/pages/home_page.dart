e2fc116bf51e4711c7b5b98ed79365358dc249bc
Zeus3hd <ahdhani0@gmail.com>
2020-08-15 14:38:55 +0300

    add the content of the main page that contains the slogan, the logo, the choices buttons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:ahdev/components/button.dart';
+import 'package:ahdev/components/logo.dart';
+import 'package:ahdev/components/slogan.dart';
 import 'package:flutter/material.dart';
 import 'package:ahdev/components/squares.dart';
 
@@ -17,6 +20,47 @@ class _HomePageState extends State<HomePage> {
           child: Stack(
             children: [
               Squares(),
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Slogan(),
+                    Hero(
+                      tag: "logo_hero",
+                      child: Logo(),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(20.0),
+                      child: Text(
+                        'I need a: ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    Button(
+                      text: 'Website',
+                      color: Color(0xffEF476F),
+                      passedIcon: Icon(
+                        Icons.desktop_mac,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                    Button(
+                      text: 'Mobile App',
+                      color: Color(0xffEF476F),
+                      passedIcon: Icon(
+                        Icons.phone_iphone,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
