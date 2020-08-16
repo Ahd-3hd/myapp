1b93b876462f4e3f588c56633e72a567f1f302d8
Zeus3hd <ahdhani0@gmail.com>
2020-08-16 11:32:03 +0300

    create the webSelectType widget

---
+++ lib/pages/web_select_type.dart
@@ -0,0 +1,35 @@
+import 'package:ahdev/components/logo.dart';
+import 'package:ahdev/components/squares.dart';
+import 'package:flutter/material.dart';
+
+class WebSelectType extends StatefulWidget {
+  WebSelectType({Key key}) : super(key: key);
+
+  @override
+  _WebSelectTypeState createState() => _WebSelectTypeState();
+}
+
+class _WebSelectTypeState extends State<WebSelectType> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Stack(
+            children: [
+              Squares(),
+              Column(
+                children: [
+                  Hero(
+                    tag: "logo_hero",
+                    child: Logo(shrink: true),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
