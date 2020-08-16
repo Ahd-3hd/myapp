93948e5038a122d83ef15300e03ebf0ab9be80fb
Zeus3hd <ahdhani0@gmail.com>
2020-08-16 11:31:24 +0300

    conditional rendering to check whether to shrink and realign the logo or not

--- lib/components/logo.dart
+++ lib/components/logo.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Logo extends StatelessWidget {
-  const Logo({Key key}) : super(key: key);
+  final bool shrink;
+  const Logo({Key key, this.shrink}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-      width: 180.0,
-      height: 45.0,
-      color: Color(0xff06D6A0),
-      child: OverflowBox(
-        minWidth: 250.0,
-        maxWidth: 250.0,
-        minHeight: 150.0,
-        maxHeight: 150.0,
-        child: Image.asset(
-          'assets/logo.png',
-          width: 200,
-        ),
-      ),
-    );
+    return shrink
+        ? Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 70, horizontal: 50),
+              width: 100.0,
+              height: 15.0,
+              color: Color(0xff06D6A0),
+              child: OverflowBox(
+                minWidth: 100.0,
+                maxWidth: 150.0,
+                minHeight: 150.0,
+                maxHeight: 150.0,
+                child: Image.asset(
+                  'assets/logo.png',
+                  width: 200,
+                ),
+              ),
+            ),
+          )
+        : Container(
+            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+            width: 180.0,
+            height: 45.0,
+            color: Color(0xff06D6A0),
+            child: OverflowBox(
+              minWidth: 250.0,
+              maxWidth: 250.0,
+              minHeight: 150.0,
+              maxHeight: 150.0,
+              child: Image.asset(
+                'assets/logo.png',
+                width: this.shrink ? 50 : 200,
+              ),
+            ),
+          );
   }
 }
